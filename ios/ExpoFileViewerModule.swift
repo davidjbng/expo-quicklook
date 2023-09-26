83ec12ebfc0850a21dc955f3ac55d553973f2042
David Jebing <29679909+davidjbng@users.noreply.github.com>
2023-09-26 15:47:34 +0200

    add ios quicklook

--- ios/ExpoFileViewerModule.swift
+++ ios/ExpoFileViewerModule.swift
@@ -1,44 +1,70 @@
-import ExpoModulesCore
-
+import Foundation  
+import QuickLook  
+import ExpoModulesCore  
+  
 public class ExpoFileViewerModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
+  private var previewController: QLPreviewController?  
+  private var dataSource: FileUrlDataSource?
+
   public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoFileViewer')` in JavaScript.
-    Name("ExpoFileViewer")
+    Name("ExpoQuicklookPreview")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ExpoFileViewerView.self) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { (view: ExpoFileViewerView, prop: String) in
-        print(prop)
+    AsyncFunction("openPreviewAsync") { (fileUrl: String, promise: Promise) in
+      guard let url = URL(string: fileUrl) else {  
+        promise.reject("E_INVALID_URL", "Invalid file URL")  
+        return  
+      }  
+      guard let filePermissions: EXFilePermissionModuleInterface =
+        appContext?.legacyModule(implementing: EXFilePermissionModuleInterface.self)
+      else {
+        throw FilePermissionModuleException()
       }
-    }
+
+      let grantedPermissions = filePermissions.getPathPermissions(url.relativePath)
+      guard grantedPermissions.rawValue >= EXFileSystemPermissionFlags.read.rawValue else {
+        throw FilePermissionException()
+      }
+
+      if self.previewController == nil {
+        self.previewController = QLPreviewController()
+      }
+
+      self.dataSource = FileUrlDataSource(fileURL: url)
+      previewController?.dataSource = self.dataSource
+
+      // TODO: pass through previewController settings
+
+      guard let currentViewcontroller = appContext?.utilities?.currentViewController() else {
+        throw MissingCurrentViewControllerException()
+      }
+
+      currentViewcontroller.present(previewController, animated: true, completion: nil)
+    }.runOnQueue(.main)
   }
 }
+
+class FileUrlDataSource: NSObject, QLPreviewControllerDataSource {  
+  var fileURL: URL?  
+
+  init(fileURL: URL?) {
+    self.fileURL = fileURL
+  }
+    
+  func numberOfPreviewItems(in controller: QLPreviewController) -> Int {  
+    return self.fileURL != nil ? 1 : 0  
+  }  
+    
+  func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {  
+    return PreviewItem(withURL: self.fileURL ?? URL(fileURLWithPath: ""))  
+  } 
+}  
+
+// TODO: do i need this
+class PreviewItem: NSObject, QLPreviewItem {  
+  var previewItemURL: URL?  
+  
+  init(withURL url: URL) {  
+    self.previewItemURL = url  
+    super.init()  
+  }  
+}  
